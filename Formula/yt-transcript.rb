277e090f300bd0e14be5c83e8729c60ec8e698e2
Will Malcomson <willfmv@gmail.com>
2026-03-04 17:27:33 +0000

    chore: bump yt-transcript to v0.1.4

--- Formula/yt-transcript.rb
+++ Formula/yt-transcript.rb
@@ -1,15 +1,15 @@
 class YtTranscript < Formula
   desc "Download model-compatible YouTube audio and transcribe locally"
   homepage "https://github.com/malcomsonbrothers/yt-transcript"
-  version "0.1.2"
+  version "0.1.4"
   license "MIT"
 
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/malcomsonbrothers/yt-transcript/releases/download/v#{version}/yt-transcript-v#{version}-macos-arm64.tar.gz"
-    sha256 "655413060e8b201b4a66d428dcf9644be994322fcb1a8101844e01137d4ea2b8"
+    sha256 "f29a1de7039144c88c4ddb7a80be26c250999ff24f7a62d3d9d1b0162617395c"
   elsif OS.linux? && Hardware::CPU.intel?
     url "https://github.com/malcomsonbrothers/yt-transcript/releases/download/v#{version}/yt-transcript-v#{version}-linux-x86_64.tar.gz"
-    sha256 "9cf05d72e8b196c6e9263253b02c85d2b4002351b046f43058f7c9c512ff94f2"
+    sha256 "8ec78298c949e0684f2592bd694e8906458cf7cd971343a6c9f86f06ced8c109"
   end
 
   head "https://github.com/malcomsonbrothers/yt-transcript.git", branch: "master"
